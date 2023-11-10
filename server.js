d8a7f1447026b21b0c9d18723fca3620c6af7e3e
Aaryan Mehta <aaryanmta@gmail.com>
2023-11-10 14:47:21 +0530

    init commit

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello chan")
+})
+
+app.listen(3000, () => {
+    console.log("App is running on port 3000");
+})
